7485e0133d5a778386bd0bc5bc0332ccdba5dd10
pioggia73 <monikawasilewska13@wp.pl>
2023-11-27 18:42:47 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Carousel from "./Carousel";
+import SlickCarousel from "./SlickCarouse";
+
+const App = () => {
+  return (
+    <main>
+      {/* <Carousel /> */}
+      <SlickCarousel />
+    </main>
+  );
+};
+export default App;
